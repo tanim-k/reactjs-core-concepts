4a7c3e015c6031f32e44e8611dc04744cacddf23
tanim-k <tanimkhan877@gmail.com>
2022-03-18 16:08:36 +0600

    reactjs basic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// global variable
+const number = 555;
+const singer = {name:'magur ali', job:'singing'}
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'cyan',
+  borderRadius: '5px'
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>JSX</h1>
+        <p>love u</p>
+        <div className="container">
+          <h3>hello dude how r you</h3>
+        </div>
+        <div className="music">
+          <h2 style={singerStyle}>name: {222 + number}</h2>
+          <h2 style={{color:'white', backgroundColor: 'goldenrod', borderRadius: '10px'}}>name: {singer.name}</h2>
+          <h2 style={singerStyle}>name: {singer.job}</h2>
+        </div>
+        <div id="countries" className="countries">
+          <p> </p>
+          <p></p>
+          <p></p>
+        </div>
+        <p style={singerStyle}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
